0b482e45b89075fa7cb82431920ea766d7ff1577
seregaxxl <seregaxxl@gmail.com>
2023-03-16 11:09:56 +0300

    CSS transition +CSSTransition group

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -3,6 +3,7 @@ import useMarvelService from '../../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
 import { useState, useLayoutEffect , useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const CharList = (props) => {
     const [chars, setChars] = useState([]);
@@ -38,25 +39,36 @@ const CharList = (props) => {
 
 
     function renderItems(chars) {
+
         const elements = chars.map((char, i) => {
+            const timeout = 500 * ((i + 1) % 9);
             const {thumbnail, name, id} = char;
             return (
-                <li className='char__item'
-                tabIndex = "0"
-                // ref={el => {
-                //     itemRefs.current[i] = el;
-                // }}
+                <CSSTransition
+                in={!loading & !newItemLoading}
+                appear={true}
                 key={id}
-                onClick={()=>{
-                    props.onCharSelected(id)
-                }}>
-                    <img src={thumbnail} style={thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? {objectFit:'contain'} : {objectFit:'cover'}} alt={name}/>
-                     <div className="char__name">{name}</div>
-                </li>
+                timeout={timeout}
+                classNames={'char__item'} >
+                        <li className='char__item'
+                        tabIndex = "0"
+                        onClick={()=>{
+                            props.onCharSelected(id)
+                        }}
+                        >
+                            <img src={thumbnail} style={thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? {objectFit:'contain'} : {objectFit:'cover'}} alt={name}/>
+                            <div className="char__name">{name}</div>
+                        </li>
+                </CSSTransition>
+                
             )
         })
         return (
-                <>{elements}</>
+            <ul className="char__grid">
+            <TransitionGroup component={null}>
+                {elements}
+            </TransitionGroup>
+        </ul>
         )
     }
 
@@ -65,13 +77,14 @@ const CharList = (props) => {
     const spiner = loading && !newItemLoading  ? <Spinner /> : null
     return  (
         <div className="char__list">
-            <ul className="char__grid">
                 {errorMessage} {spiner} {items}
-            </ul>
             <button onClick={() => {onLoadMoreChars(offset)}} className="button button__main button__long" style={loading || charEnded || newItemLoading  ? {display: "none"} : {display:"block"}}>
             
                 <div className="inner">load more</div>
             </button>
+            {/* <button onClick={() => {setAppear(true)}} className="button button__main button__long">
+                <div className="appear">appear</div>
+            </button> */}
         </div>
     ) 
 }
